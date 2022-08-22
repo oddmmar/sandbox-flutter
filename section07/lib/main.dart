bd5cefee03ba8a39f9b8032cf75ed8e7742c944d
odwamajila <oddmmar@gmail.com>
2022-08-22 20:01:15 +0200

    section 07: starts meals app with navigation

---
+++ section07/lib/main.dart
@@ -0,0 +1,23 @@
+// import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:section07/routes/home_screen.dart';
+// import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const Meals());
+}
+
+class Meals extends StatelessWidget {
+  const Meals({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
