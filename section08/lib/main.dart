b98af4bb830153633d685c7b761c11b63d12585a
odwamajila <oddmmar@gmail.com>
2022-09-03 23:03:24 +0200

    adds small theming

--- section08/lib/main.dart
+++ section08/lib/main.dart
@@ -10,12 +10,16 @@ class MyShop extends StatelessWidget {
 
   final appTheme = ThemeData(
     primarySwatch: Colors.brown,
+    fontFamily: 'Lato',
   );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: appTheme,
+      theme: appTheme.copyWith(
+        colorScheme:
+            appTheme.colorScheme.copyWith(secondary: Colors.amberAccent),
+      ),
       home: ProductOverviewScreen(),
     );
   }
